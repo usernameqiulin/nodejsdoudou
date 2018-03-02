03c6407366c02ffbe36f42e66881569f34a0ad05
unknown <419444604@qq.com>
2018-03-02 17:35:52 +0800

    添加商品到数据库

--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var md5 = require("md5");
 var UserModel = require("../model/UserModel");
+var GoodsModel = require("../model/GoodsModel");
 
 /* GET home page. */
 
@@ -51,6 +52,43 @@ router.post("/api/login", function(req, res) {
 		}
 	})
 })
+router.post("/api/new", function(req, res){
+	var result = {
+		status: 1,
+		message: "已保存到数据库"
+	}
+	// express post参数接收。
+	// 所有的post参数都被包装到req.body中
+	var shopname = req.body.shopname;
+	var shopprice = req.body.shopprice;
+	
+	 // GoodsModel.find({shopname: shopname,shopprice: shopprice}, function(err, docs) {
+ 	// 	 var um = new GoodsModel();
+ 	// 	 alert(um.shopname);
+ 	// 	 alert(um.shopprice);
+ 	// })
+
+
+
+		// 保存功能 (mongodb的调用使用mongoose组件)
+		var um = new GoodsModel();
+		um.shopname = shopname;
+		um.shopprice = shopprice;
+		um.save(function(err){
+			if(!err) {
+				console.log("已保存到数据库");
+				result.status = -110;
+				result.message = "已保存到数据库";
+				res.send(result);
+			} 
+		})
+		/*var query = GoodsModel.collection({shopname,shopprice},function(err,docs){
+			alert(docs);
+
+		});*/
+
+	})
+
 
 
 
