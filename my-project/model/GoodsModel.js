76f1097d7eeabec661142cd76023d6c10e5bb577
unknown <419444604@qq.com>
2018-03-02 20:13:48 +0800

    从服务器请求数据提交

--- my-project/model/GoodsModel.js
+++ my-project/model/GoodsModel.js
@@ -2,13 +2,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 // 创建文档的定义
-var User = new Schema({
+var Goods = new Schema({
     shopname  : String,
     shopprice       : String,
     create_date : { type: Date, default: Date.now }
 });
 
 // 创建model对象，与数据库中的文档（表）映射
-var UserModel = mongoose.model('user', User);
+var GoodsModel = mongoose.model('goods', Goods);
 // commonJS规范(暴露接口)
-module.exports = UserModel;
+module.exports = GoodsModel;
--- my-project/public/js/new.js
+++ my-project/public/js/new.js
@@ -1,11 +1,27 @@
-$(".tab-div-new p span").click(function(){
-	$(this).addClass('tab-front')
-		.siblings()
-		.removeClass('tab-front');
-var index = $(this).index();
-$(".form-table01").eq( index ).css("display","table")
-							.siblings()
-							.css("display","none");
-			$(".button-div").css("display","block");
+$(".tab-div-new p span").click(function() {
+		$(this).addClass('tab-front')
+			.siblings()
+			.removeClass('tab-front');
+		var index = $(this).index();
+		$(".form-table01").eq(index).css("display", "table")
+			.siblings()
+			.css("display", "none");
+		$(".button-div").css("display", "block");
 
-});
+	});
+$(".button").click(function(){
+	$.ajax({
+			url: "/api/new",
+			type: "post",
+			data: {
+				shopname: $(".left").val(),
+				shopprice: $(".shopprice").val()
+			},
+			success: function(res) {
+				// query.exec(docs){
+				console.log(res);
+				// }
+				}
+		
+		})
+})
